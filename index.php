72dda71bb3d78000c79ff2635167436626b04c5c
Saida Ishchanova <saidamedviju@gmail.com>
2020-12-20 19:07:27 +0500

    dz_18/12/2020

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>PHP</title>
+
+</head>
+<body>
+    <?php
+
+    $str_1 = "<i>Ищанова Саида</i>";
+    $str_2 = "<b>30 лет</b>";
+    echo $str_1." ".$str_2. "<br /><hr />";
+    define("PI",3.1415926535);
+    echo PI;
+
+    ?>
+</body>
+</html>
